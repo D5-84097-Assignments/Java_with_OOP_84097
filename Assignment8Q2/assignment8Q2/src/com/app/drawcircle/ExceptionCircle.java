f18c2a92cc4a71b830b177bff12ca7f80af7c176
Amit-Raskar <amitraskar09@gmail.com>
2024-05-01 11:04:15 +0530

    Assignment 8 Q2

---
+++ Assignment8Q2/assignment8Q2/src/com/app/drawcircle/ExceptionCircle.java
@@ -0,0 +1,10 @@
+package com.app.drawcircle;
+
+public class ExceptionCircle extends Exception{
+	
+	public ExceptionCircle() {
+	}
+	public ExceptionCircle(String message){
+		super(message);
+	}
+}
